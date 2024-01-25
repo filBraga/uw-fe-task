04ce5557314d0c423f115c6dfbd7a2746a200131
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-25 11:06:03 -0300

    feat: adding countries

---
+++ app/utils/countries.ts
@@ -0,0 +1,24 @@
+import { CountryType } from '../types';
+
+export const countries: CountryType[] = [
+    { code: 'AR', label: 'Argentina', regex: /argentina/i },
+    { code: 'AU', label: 'Australia', regex: /asdasdasdadsasd/i },
+    { code: 'BR', label: 'Brazil', regex: /asdasdasdadsasd/i },
+    { code: 'CA', label: 'Canada', regex: /^[ABD0-9]{10}-[A-Z]{2}$/i },
+    { code: 'CH', label: 'Switzerland', regex: /asdasdasdadsasd/i },
+    { code: 'CL', label: 'Chile', regex: /asdasdasdadsasd/i },
+    { code: 'CN', label: 'China', regex: /asdasdasdadsasd/i },
+    { code: 'DE', label: 'Germany', regex: /asdasdasdadsasd/i },
+    { code: 'ES', label: 'Spain', regex: /asdasdasdadsasd/i },
+    { code: 'GB', label: 'United Kingdom', regex: /asdasdasdadsasd/i },
+    { code: 'IE', label: 'Ireland', regex: /asdasdasdadsasd/i },
+    { code: 'IN', label: 'India', regex: /asdasdasdadsasd/i },
+    { code: 'IT', label: 'Italy', regex: /asdasdasdadsasd/i },
+    { code: 'JP', label: 'Japan', regex: /asdasdasdadsasd/i },
+    { code: 'MX', label: 'Mexico', regex: /asdasdasdadsasd/i },
+    { code: 'PT', label: 'Portugal', regex: /asdasdasdadsasd/i },
+    { code: 'RU', label: 'Russia', regex: /asdasdasdadsasd/i },
+    { code: 'US', label: 'United States', regex: /^\d{4}-[A-Z]{3}-\d{5}(\d{2})?$/i },
+    { code: 'UY', label: 'Uruguay', regex: /asdasdasdadsasd/i },
+    { code: 'ZA', label: 'South Africa', regex: /asdasdasdadsasd/i },
+];
