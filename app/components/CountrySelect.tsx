4c603202365f15f5710aa96691317293e4931539
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-25 11:05:39 -0300

    feat: country select

---
+++ app/components/CountrySelect.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import { CountryType } from '../types';
+
+interface CountrySelectProps {
+    countries: CountryType[];
+    onChange: (country: CountryType | null) => void;
+}
+
+export default function CountrySelect({ countries, onChange }: CountrySelectProps) {
+    return (
+        <Autocomplete
+            id="country-select-demo"
+            sx={{ width: 300 }}
+            options={countries}
+            autoHighlight
+            getOptionLabel={(option) => option.label}
+            onChange={(event, newValue) => {
+                onChange(newValue);
+            }}
+            renderOption={(props, option) => (
+                <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+                    <img
+                        loading="lazy"
+                        width="20"
+                        srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+                        src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+                        alt=""
+                    />
+                    {option.label} ({option.code})
+                </Box>
+            )}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Choose a country"
+                    inputProps={{
+                        ...params.inputProps,
+                        autoComplete: 'new-password', // disable autocomplete and autofill
+                    }}
+                />
+            )}
+        />
+    );
+}
