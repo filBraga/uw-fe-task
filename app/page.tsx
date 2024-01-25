aa545b44f244575dafeddd64925c6dea344c85fc
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-25 11:22:07 -0300

    feat: add name validation

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,13 @@ import { useState } from 'react';
 import { countries } from './utils/countries';
 
 export default function Home() {
+    const [name, setName] = useState<string>('');
     const [selectedCountry, setSelectedCountry] = useState<CountryType | null>(null);
     const [taxId, setTaxId] = useState<string>('');
+
+    const isNameValid = () => {
+        return name.length >= 3;
+    };
 
     const isTaxIdValid = () => {
         if (!selectedCountry || !taxId) return false;
@@ -20,18 +25,27 @@ export default function Home() {
     return (
         <main className={styles.main}>
             <Paper className={styles.card} elevation={3}>
-                <TextField className={styles.input} id="outlined-basic" label="Name" variant="outlined" />
+                <TextField
+                    className={styles.input}
+                    id="name"
+                    label="Name"
+                    variant="outlined"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    error={!isNameValid() && name.length > 0}
+                    helperText={!isNameValid() && taxId.length > 0 ? 'Minimum name length: 3 characters' : ''}
+                />
                 <CountrySelect countries={countries} onChange={(country) => setSelectedCountry(country)} />
                 <TextField
                     className={styles.input}
-                    id="outlined-basic"
+                    id="tax"
                     label="Tax ID"
                     variant="outlined"
                     value={taxId}
                     onChange={(e) => setTaxId(e.target.value)}
                     error={!isTaxIdValid() && taxId.length > 0}
                     helperText={!isTaxIdValid() && taxId.length > 0 ? 'Invalid Tax ID for selected country' : ''}
-                />{' '}
+                />
             </Paper>
         </main>
     );
